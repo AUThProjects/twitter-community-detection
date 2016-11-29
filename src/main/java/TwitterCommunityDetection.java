1cb44d7184c1b3b6795604b2ef3b5c6c9977a189
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-11-29 17:24:52 +0100

    Add logger

--- src/main/java/TwitterCommunityDetection.java
+++ src/main/java/TwitterCommunityDetection.java
@@ -2,6 +2,7 @@
  * Program entry point
  * */
 
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
@@ -14,6 +15,7 @@ import twitter4j.conf.ConfigurationContext;
 
 public class TwitterCommunityDetection {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(TwitterCommunityDetection.class.getClass());
         SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("TwitterStreamingInput");
         JavaStreamingContext  jssc = new JavaStreamingContext(conf, new Duration(5000));
         Configuration twitterConf = ConfigurationContext.getInstance();
