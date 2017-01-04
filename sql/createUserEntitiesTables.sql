e30ea3804f652d7a57193bce4e84b64ecd847643
Antonios Anagnostou <anagnoad@csd.auth.gr>
2017-01-04 19:34:22 +0200

    Change db schema

--- sql/createUserEntitiesTables.sql
+++ sql/createUserEntitiesTables.sql
@@ -4,24 +4,24 @@ USE DATABASE twitterDB;
 
 CREATE TABLE user_hashtag(
   id serial primary key,
-  uid int not null,
+  uid bigint not null,
   hashtag varchar(50) not null
 )
 
 CREATE TABLE user_url(
   id serial primary key,
-  uid int not null,
+  uid bigint not null,
   url varchar(255)
 )
 
 CREATE TABLE user_mention(
   id serial primary key,
-  uid int not null,
-  mention_uid int not null
+  uid bigint not null,
+  mention_id bigint not null
 )
 
 CREATE TABLE user_retweet(
   id serial primary key,
   uid int not null,
-  retweet_uid int not null
+  retweet_id bigint not null
 )
