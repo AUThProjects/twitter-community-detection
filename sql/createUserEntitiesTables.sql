7a298c320755f0cea57dbb197e01f324b242109d
Steve Laskaridis <stevelaskaridis@gmail.com>
2017-01-04 19:38:29 +0200

    Add semi-colons to sql

--- sql/createUserEntitiesTables.sql
+++ sql/createUserEntitiesTables.sql
@@ -6,22 +6,22 @@ CREATE TABLE user_hashtag(
   id serial primary key,
   uid varchar(100) not null,
   hashtag varchar(50) not null
-)
+);
 
 CREATE TABLE user_url(
   id serial primary key,
   uid varchar(100) not null,
   url varchar(255)
-)
+);
 
 CREATE TABLE user_mention(
   id serial primary key,
   uid varchar(100) not null,
   mention_id varchar(100) not null
-)
+);
 
 CREATE TABLE user_retweet(
   id serial primary key,
   uid varchar(100) not null,
   retweet_id varchar(100) not null
-)
+);
