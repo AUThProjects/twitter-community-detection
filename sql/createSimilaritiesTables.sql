2242f43d8d39405640dca13a96269574c3edaac8
Steve Laskaridis <stevelaskaridis@gmail.com>
2017-01-14 18:38:40 +0200

    Add cosine similarity tables per field

--- sql/createSimilaritiesTables.sql
+++ sql/createSimilaritiesTables.sql
@@ -1,6 +1,27 @@
 USE DATABASE twitterdb;
 
-CREATE TABLE cosine_similarity (
+CREATE TABLE cosine_similarity_hashtag (
+  id serial primary key,
+  uid_r varchar(100) not null,
+  uid_c varchar(100) not null,
+  similarity double precision not null
+);
+
+CREATE TABLE cosine_similarity_url (
+  id serial primary key,
+  uid_r varchar(100) not null,
+  uid_c varchar(100) not null,
+  similarity double precision not null
+);
+
+CREATE TABLE cosine_similarity_mention (
+  id serial primary key,
+  uid_r varchar(100) not null,
+  uid_c varchar(100) not null,
+  similarity double precision not null
+);
+
+CREATE TABLE cosine_similarity_retweet (
   id serial primary key,
   uid_r varchar(100) not null,
   uid_c varchar(100) not null,
