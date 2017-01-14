f97e34f3d5778241168bc9c4eaa4c3c55043dd7c
Steve Laskaridis <stevelaskaridis@gmail.com>
2017-01-14 18:39:23 +0200

    Compute cosine similarities per field

--- src/main/java/SimilarityComputation.java
+++ src/main/java/SimilarityComputation.java
@@ -12,6 +12,13 @@ import static java.lang.Math.sqrt;
 public class SimilarityComputation {
 
     public static void main (String[] args) {
+        computeCosineSimilarity("hashtag", "user_hashtag");
+        computeCosineSimilarity("url", "user_url");
+        computeCosineSimilarity("retweet_id", "user_retweet");
+        computeCosineSimilarity("mention_id", "user_mention");
+    }
+
+    public static void computeCosineSimilarity(String column, String table) {
 
         HashMap<String, Double> magnitudes = new HashMap<>();
         HashMap<Tuple2<String, String>, Double> similarities = new HashMap<>();
@@ -29,14 +36,9 @@ public class SimilarityComputation {
                     "jdbc:postgresql://localhost:5432/twitterdb", "twitteruser", "twitterpass");
 
             Statement stmt = connection.createStatement();
-            String hashtagCountSql = "SELECT count(distinct hashtag) FROM user_hashtag";
-            ResultSet rs = stmt.executeQuery(hashtagCountSql);
-            rs.next();
-            int totalHashtags = rs.getInt("count");
-            System.out.println(totalHashtags);
             stmt = connection.createStatement();
-            String sql = "select hashtag, uid, count(uid) as countrow from user_hashtag group by hashtag, uid order by uid";
-            rs = stmt.executeQuery(sql);
+            String sql = String.format("select %1$s, uid, count(uid) as countrow from %2$s group by %1$s, uid order by uid", column, table);
+            ResultSet rs = stmt.executeQuery(sql);
             double acc = 0;
             String previousUid = "";
             while (rs.next()) {
@@ -55,24 +57,23 @@ public class SimilarityComputation {
             }
 //            System.out.println(magnitudes.size());
 
-            String similarSql = "select uid1, uid2, hashtag, user1_hashtag_count, user2_hashtag_count\n" +
+            String similarSql = String.format("select uid1, uid2, %2$s, user1_%2$s_count, user2_%2$s_count\n" +
                     "from (\n" +
-                    "  select uh1.uid as uid1, uh2.uid as uid2, uh1.hashtag as hashtag, uh1.count as user1_hashtag_count, uh2.count as user2_hashtag_count\n" +
+                    "  select uh1.uid as uid1, uh2.uid as uid2, uh1.%2$s as %2$s, uh1.count as user1_%2$s_count, uh2.count as user2_%2$s_count\n" +
                     "  from (\n" +
-                    "    select uh1.uid, uh1.hashtag, count(uh1.hashtag)\n" +
-                    "    from user_hashtag as uh1\n" +
-                    "    group by uh1.uid, uh1.hashtag  \n" +
+                    "    select uh1.uid, uh1.%2$s, count(uh1.%2$s)\n" +
+                    "    from %1$s as uh1\n" +
+                    "    group by uh1.uid, uh1.%2$s  \n" +
                     "  ) as uh1\n" +
                     "  inner join (\n" +
-                    "  select uh2.uid, uh2.hashtag, count(uh2.hashtag)\n" +
-                    "  from user_hashtag as uh2 \n" +
-                    "  group by uh2.uid, uh2.hashtag\n" +
+                    "  select uh2.uid, uh2.%2$s, count(uh2.%2$s)\n" +
+                    "  from %1$s as uh2 \n" +
+                    "  group by uh2.uid, uh2.%2$s\n" +
                     "  ) as uh2\n" +
-                    "  on uh1.hashtag=uh2.hashtag \n" +
+                    "  on uh1.%2$s=uh2.%2$s \n" +
                     "  where uh1.uid < uh2.uid\n" +
                     "  order by uh1.uid asc, uh2.uid asc\n" +
-                    ") as q\n" +
-                    "\n";
+                    ") as q\n", table, column);
             stmt = connection.createStatement();
             rs = stmt.executeQuery(similarSql);
 
@@ -80,8 +81,8 @@ public class SimilarityComputation {
             String uid2 = "";
             acc = 0;
             while (rs.next()) {
-               String nextUid1 = rs.getString("uid1");
-               String nextUid2 = rs.getString("uid2");
+                String nextUid1 = rs.getString("uid1");
+                String nextUid2 = rs.getString("uid2");
 //                System.out.println(nextUid1 + " " + nextUid2);
 //                System.out.println(magnitudes.get(nextUid1));
 //                for (Map.Entry<String, Double> entry: magnitudes.entrySet()) {
@@ -93,17 +94,17 @@ public class SimilarityComputation {
                     acc = 0;
                 }
 
-               int count1= rs.getInt("user1_hashtag_count");
-               int count2= rs.getInt("user2_hashtag_count");
-               acc += count1*count2;
+                int count1= rs.getInt(String.format("user1_%1$s_count", column));
+                int count2= rs.getInt(String.format("user2_%1$s_count", column));
+                acc += count1*count2;
 
-               uid1=nextUid1;
-               uid2=nextUid2;
+                uid1=nextUid1;
+                uid2=nextUid2;
             }
 
             for (Map.Entry<Tuple2<String, String>, Double> entry: similarities.entrySet()) {
                 stmt = connection.createStatement();
-                String insertSimilarity = String.format("INSERT INTO cosine_similarity(uid_r,uid_c,similarity) VALUES(%s, %s, %f)", entry.getKey()._1(), entry.getKey()._2(), entry.getValue());
+                String insertSimilarity = String.format("INSERT INTO cosine_similarity_%3$s(uid_r,uid_c,similarity) VALUES(%s, %s, %f)", entry.getKey()._1(), entry.getKey()._2(), entry.getValue(), table);
                 stmt.executeUpdate(insertSimilarity);
             }
         }
